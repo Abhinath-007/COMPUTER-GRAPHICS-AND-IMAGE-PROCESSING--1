c607ac7353ccc2516f47d69388837b1d36e655c4
Abhinath-007 <abhinath944744@gmail.com>
2026-03-28 21:53:58 +0530

    Update assg.py

--- assg.py
+++ assg.py
@@ -3,48 +3,47 @@ import matplotlib.pyplot as plt
 # Bresenham's Line Drawing Algorithm
 # -------------------------------
 def bresenham_line(x1, y1, x2, y2):
-points = []
-dx = abs(x2 - x1)
-dy = abs(y2 - y1)
-sx = 1 if x2 > x1 else -1
-sy = 1 if y2 > y1 else -1
-err = dx - dy
-while True:
-points.append((x1, y1))
-if x1 == x2 and y1 == y2:
-break
-e2 = 2 * err
-if e2 > -dy:
-err -= dy
-x1 += sx
-if e2 < dx:
-err += dx
-y1 += sy
-return points
+  points = []
+  dx = abs(x2 - x1)
+  dy = abs(y2 - y1)
+  sx = 1 if x2 > x1 else -1
+  sy = 1 if y2 > y1 else -1
+  err = dx - dy
+  while True:
+    points.append((x1, y1))
+    if x1 == x2 and y1 == y2:
+    break
+    e2 = 2 * err
+    if e2 > -dy:
+    err -= dy
+    x1 += sx
+    if e2 < dx:
+    err += dx
+    y1 += sy
+  return points
 # -------------------------------
 # Bresenham's Circle Drawing Algorithm
 # -------------------------------
 def bresenham_circle(xc, yc, r):
-points = []
-x = 0
-y = r
-d = 3 - 2 * r
-while x <= y:
-# 8-way symmetry
-points.extend([
-(xc + x, yc + y), (xc - x, yc + y),
-(xc + x, yc - y), (xc - x, yc - y),
-(xc + y, yc + x), (xc - y, yc + x),
-(xc + y, yc - x), (xc - y, yc - x)
-])
-if d < 0:
-
-d = d + 4 * x + 6
-else:
-d = d + 4 * (x - y) + 10
-y -= 1
-x += 1
-return points
+  points = []
+  x = 0
+  y = r
+  d = 3 - 2 * r
+  while x <= y:
+    # 8-way symmetry
+    points.extend([
+    (xc + x, yc + y), (xc - x, yc + y),
+    (xc + x, yc - y), (xc - x, yc - y),
+    (xc + y, yc + x), (xc - y, yc + x),
+    (xc + y, yc - x), (xc - y, yc - x)
+    ])
+    if d < 0:
+      d = d + 4 * x + 6
+    else:
+      d = d + 4 * (x - y) + 10
+      y -= 1
+    x += 1
+  return points
 # -------------------------------
 # Drawing the Car
 # -------------------------------
